208b82427ae595c2a26c92e05ef667c51f7458af
luizdezorze <63805926+luizdezorze@users.noreply.github.com>
2020-06-17 17:39:34 -0300

    Update my.js

--- my.js
+++ my.js
@@ -13,3 +13,30 @@ fetch(url)
 }
 
 document.getElementById("btn").addEventListener("click",testGS);
+
+
+
+
+
+function addGS() {
+
+    const url = "https://script.google.com/macros/s/AKfycbwhkS-jawL7rjVPgrdBFt4o05jF1krFGIPhVorqwKsRY9-qJp0E/exec";
+
+
+fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow', // manual, *follow, error
+    body: JSON.stringify({name: "Joao"}) // body data type 
+  });
+    
+}
+
+document.getElementById("btn2").addEventListener("click",addGS)
+;
+document.getElementById("btn").addEventListener("click",testGS);
+
